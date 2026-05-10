4396327d1f270a13ab447bf86774d6a274db084b
u23734176-del <u23734176@tuks.co.za>
2026-05-10 19:18:15 +0200

    Fix and and Safery for Navagation to work in Docker Conaier

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,16 +1,16 @@
 <!-- Tadiwanashe Chigeza -->
  <!-- u23734176 -->
 <script setup>
-  import index from './pages/index.vue';
-
+  import Navigation from './components/Navigation.vue'
 </script>
 <template>
   <div>
     <header>
       <h1>My Blog Website</h1>
-      
+
       <Navigation />
     </header>
+
     <NuxtPage />
   </div>
 </template>
